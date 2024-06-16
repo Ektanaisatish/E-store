f74b815e736a1c59d03e4c659406592ac130f1f2
Nai-Ekta-Satish-kumar <ektan@zignuts.com>
2024-06-16 17:26:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Products from "./views/Products";
+
+import Mycart from "./views/Mycart";
+import { Toaster } from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+import { loginSuccess } from "./redux/authSlice.js";
+import SignUpForm from "./views/Auth/SignUpForm.js";
+import LoginForm from "./views/Auth/LoginForm.js";
+
+function App() {
+  const dispatch = useDispatch();
+  const loggedInUser = useSelector((state) => state.auth.loggedInUser);
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("loggedInUser"));
+    if (user) {
+      dispatch(loginSuccess(user));
+    }
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      <React.Fragment>
+        <Navbar />
+        <Routes>
+          {!loggedInUser ? (
+            <>
+              <Route path="/signup" element={<SignUpForm />} />
+              <Route path="/" element={<LoginForm />} />
+            </>
+          ) : (
+            <>
+              <Route path="/mycart" element={<Mycart />} />
+            </>
+          )}
+          <Route path="/products" element={<Products />} />
+        </Routes>
+        <Toaster />
+      </React.Fragment>
+    </div>
+  );
+}
+export default App;
